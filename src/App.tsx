e85f649aa7d6bd1ed551e10b18dff47d6199aa93
Jung-Hua Liu <jung-hua.liu@ocrlabs.com>
2022-10-20 10:30:20 +0800

    Update sdk client key

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
 
 <PaperSDKProvider appName='Paper Checkout'>
   <CheckoutWithCard
-    sdkClientSecret='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250cmFjdElkIjoiOTE0ZDZjM2ItMWY2Ny00NWU1LTk2OTQtYzQxNzBiMmM4NjhiIiwid2FsbGV0QWRkcmVzcyI6IjB4YzgxODZhMzA0NEQzMTFlZWMxQzFiNTczNDJBYWEyOTBGNmQ5MEFhNSIsImVtYWlsIjoibm8tcmVwbHlAcGFwZXIueHl6IiwicXVhbnRpdHkiOjEsInVzZVBhcGVyS2V5IjpmYWxzZSwibWV0YWRhdGEiOnt9LCJleHBpcmVzSW5NaW51dGVzIjoxNSwiaGlkZUFwcGxlUGF5R29vZ2xlUGF5IjpmYWxzZSwidGl0bGUiOiJIZWxsbyBXb3JsZCIsImNhcHR1cmVQYXltZW50TGF0ZXIiOmZhbHNlLCJtaW50TWV0aG9kIjp7Im5hbWUiOiJjbGFpbVRvIiwiYXJncyI6eyJfdG8iOiIkV0FMTEVUIiwiX3F1YW50aXR5IjoiJFFVQU5USVRZIiwiX3Rva2VuSWQiOjB9LCJjYWxsT3B0aW9ucyI6eyJnYXNQcmlvcml0eSI6Im1lZGl1bSJ9LCJwYXltZW50Ijp7ImN1cnJlbmN5IjoiTUFUSUMiLCJ2YWx1ZSI6IjAuMDAwMSAgKiAkUVVBTlRJVFkifX0sInByaWNpbmdEZXRhaWxzIjp7ImNoYWluTmFtZSI6Ik11bWJhaSIsImN1cnJlbmN5QWRkcmVzcyI6IjB4MDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsImN1cnJlbmN5U3ltYm9sIjoiTUFUSUMiLCJyZWFkYWJsZVByaWNlIjoiMC4wMDAxIiwicHJpY2VJbldlaSI6eyJ0eXBlIjoiQmlnTnVtYmVyIiwiaGV4IjoiMHg1YWYzMTA3YTQwMDAifSwibG9ja2VkUHJpY2VVc2RDZW50cyI6MH0sInRyYW5zYWN0aW9uSWQiOiI4M2U2MTJmMC0xODZjLTQ4MmMtOGIxZC0wMzA0N2IyMDMwODMiLCJpYXQiOjE2NjU2NzMyMTEsImV4cCI6MTY2NTY3NDExMSwiaXNzIjoicGFwZXIueHl6In0.Z5kC2Dkxf0k4y7-V9qq1u4EwvBUIWCmp3Qk_1OjGbgM'
+    sdkClientSecret='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250cmFjdElkIjoiYTc0MTA5NDMtZWVkOS00MTBkLWExOWYtMDk1ZGE3ZTE2NTA1Iiwid2FsbGV0QWRkcmVzcyI6IjB4Njk1ZUIwMmY3NTc0NGNkOTc5ZmEwMEM1QkREZTUxQTIyMjJDYTU2QSIsImVtYWlsIjoiZ3dyeDIwMDVAZ21haWwuY29tIiwicXVhbnRpdHkiOjExMTEsInVzZVBhcGVyS2V5IjpmYWxzZSwibWV0YWRhdGEiOnt9LCJleHBpcmVzSW5NaW51dGVzIjoxNTAwMDAwLCJoaWRlQXBwbGVQYXlHb29nbGVQYXkiOmZhbHNlLCJ1c2VDaGVja291dERvdENvbSI6ZmFsc2UsInRpdGxlIjoiIiwiY2FwdHVyZVBheW1lbnRMYXRlciI6ZmFsc2UsInByaWNpbmdEZXRhaWxzIjp7InJlYWRhYmxlUHJpY2UiOiIwLjAiLCJjaGFpbk5hbWUiOiJNdW1iYWkiLCJwcmljZUluV2VpIjp7InR5cGUiOiJCaWdOdW1iZXIiLCJoZXgiOiIweDAwIn0sImN1cnJlbmN5QWRkcmVzcyI6IjB4MDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMCIsImN1cnJlbmN5U3ltYm9sIjoiTUFUSUMiLCJsb2NrZWRQcmljZVVzZENlbnRzIjowfSwidHJhbnNhY3Rpb25JZCI6IjRhNjY3OGY4LWE0MTEtNDcxMS05OTFmLTUxOTJhMjJmN2QxNiIsImlhdCI6MTY2NTk3NDMyOCwiZXhwIjoxNzU1OTc0MzI4LCJpc3MiOiJwYXBlci54eXoifQ.Je9YnKdiDanOekuZae5mr5LS19qyFFkAAiVsShMnXGg'
     onPaymentSuccess={(result) => {}}
     onReview={(result) => {}}
     onError={(error: PaperSDKError) => {}}
